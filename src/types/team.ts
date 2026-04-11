edd2e9c9e7fbc013c204f8da23762a8ed889a3ec
Gerard Moliner <79200478+Mooliner@users.noreply.github.com>
2026-04-11 16:44:36 +0200

    Fix: error TeamEntity id duplicate

--- src/types/team.ts
+++ src/types/team.ts
@@ -4,7 +4,6 @@ export interface TeamEntity {
     uri?: string;
     id?: string;
     name?: string;
-    id?: string;
     city?: string;
     category?: string;
     educationalCenter?: string | null;
